174741ccff3d1a1a72001c206dffc213c99cd906
julzdoug <jschwarzzh@hotmail.com>
2023-03-28 16:43:09 +0200

    title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+<<<<<<< HEAD
     @if(Request::path() === "General_Events")
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -50,4 +51,30 @@
         @include('publications.partials.publicationEdit')
     @endif
     @include('publications.publication') 
+=======
+    <x-slot name="header">
+            </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                    
+                    <form action="{{ route('add-comment') }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <textarea class="form-control" id="title" name="title" rows="1" required></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="comment" class="form-label">Comment</label>
+                            <textarea class="form-control" id="comment" name="comment" rows="3" required></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+>>>>>>> fa66b06 (title)
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware('auth')->group(function () {
 });
 
 
+<<<<<<< HEAD
 
 
 
@@ -111,3 +112,7 @@ Route::middleware('auth')->group(function () {
 
 
 require __DIR__.'/auth.php';
+=======
+Route::post('/add-comment', 'App\Http\Controllers\PublicationDetailsController@addComment')->name('add-comment');
+Route::post('/add-title', 'App\Http\Controllers\PublicationDetailsController@addtitle')->name('add-title');
+>>>>>>> fa66b06 (title)
