f31a34cfb9a1ac01290be451e9f0c13bd716c607
MayckellP <mayckell-perez@hotmail.com>
2023-03-30 14:14:05 +0200

    design

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,43 +1,21 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('HOME') }}
-        </h2>
-    </x-slot>
-    <div class="w-75 m-auto my-3">
-        <div class="row">
-            <div class="col-sm-3 mb-3 mb-sm-0">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">GENERAL EVENTS</h5>
-                        <a href="/General_Events" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-3 mb-3 mb-sm-0">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">RESTAURANTS EVENT</h5>
-                        <a href="/Food_Events" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-3 mb-3 mb-sm-0">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">PUBS EVENTS</h5>
-                        <a href="/Pub_Events" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-3 mb-3 mb-sm-0">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">SMALLS EVENTS</h5>
-                        <a href="/Smalls_Events" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
+    <style>
+        .cont-general{
+            height:91vh;
+            background-color:gray;
+            display: flex;
+            justify-content:center;
+            align-items:center;
+        }
+        .cont-img{
+            border:1px solid black;
+            margin-bottom:10rem;
+        }
+        .cont-img img{}
+    </style>
+    <div class="cont-general">
+        <div class="cont-img">
+            <img src="/images/logo.svg" alt="">
         </div>
     </div>
 </x-app-layout>
