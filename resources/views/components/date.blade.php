688d5ee7e559a5dcb5afde12890521cfd2b056e6
MayckellP <mayckell-perez@hotmail.com>
2023-04-03 14:47:17 +0200

    Save

---
+++ resources/views/components/date.blade.php
@@ -0,0 +1,8 @@
+<style>
+    .date{
+        border-radius: 10px;
+        width:60%;
+    }
+</style>
+
+    <input type="date" id="date" class="date">
---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,12 @@
+<style>
+    .cont-search{
+        width:55%;
+        display:flex;
+    }
+</style>
+<div class="cont-search">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success p-1" type="submit">
+        <i class="bi bi-search"></i>
+    </button>
+</div>
