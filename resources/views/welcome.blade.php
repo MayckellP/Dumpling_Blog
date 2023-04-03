fa50b368802bd33b1ed66f3e95000b02818a60cf
julzdoug <jschwarzzh@hotmail.com>
2023-04-03 10:24:04 +0200

    navbar tryout 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-        <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        
+            <script src=
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js">
+            </script>
+            <script src=
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
+            </script>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.scss'])
@@ -16,11 +22,17 @@
     <body class="antialiased">
         <nav class="navbar bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="">
-                    <img src="{{asset('images/dumplingLogoWithSigns-min.svg') }}" alt="dumpling blog logo" width="70" height="70">
-                  </a>
+                <span class="navbar-brand mb-0 h1">
+                    <img src="{{asset('images/justDumplingLogo.png') }}" alt="dumpling blog logo" width="150" height="70">
+                </span>
            
-        
+             <button class="navbar-toogle ms-auto" type="button" data-toggle="collapse" data-target="Menu">
+                <i class="fa fa-bars"></i>
+             </button>
+            </div>
+        </nav>   
+        <div class="collapse" id="Menu">
+            <div class="bg-light shadow-3
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                     @auth
@@ -35,7 +47,6 @@
                 </div>
             @endif
         
-    </div>
-</nav>   
+
     </body>
 </html>
