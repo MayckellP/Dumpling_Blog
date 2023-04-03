04dc2bf9aa58b9f25b1b91bb9826d9e3d1e72c74
julzdoug <jschwarzzh@hotmail.com>
2023-04-03 14:44:22 +0200

    navbar error

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,6 +19,7 @@
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
+                    
                 </div>
             </div>
         </div>
