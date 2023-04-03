9ce031105083bf64a707f05d9b5d66db0882cfb2
julzdoug <jschwarzzh@hotmail.com>
2023-04-03 16:45:14 +0200

    text color

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,22 +7,22 @@
         <div class="offcanvas offcanvas-end shadow-5-strong" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <button type="button" class="btn-close bg-primary" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
 
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ url('/homePage') }}"><i class="bi bi-house"></i>Home</a>
+                        <a class="nav-link active text-dark" aria-current="page" href="{{ url('/homePage') }}"><i class="bi bi-house"></i>Home</a>
                     </li>
                     <li class="nav-item">
                         @auth
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
+                        <a class="nav-link text-dark" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
                     </li>
                     <li class="nav-item">
                         @if (Route::has('register'))
-                        <a class="nav-link" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
+                        <a class="nav-link text-dark" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
                         @endif
                     </li>
                     @else
