2ed46519306ac1250a531b35f03be902e4eb8665
julzdoug <jschwarzzh@hotmail.com>
2023-04-03 16:40:22 +0200

    navbar transparent

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-dark shadow-5-strong">
     <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="{{asset('images/justDumplingLogo.png') }}" alt="dumpling blog logo" width="150" height="70"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+        <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+        <div class="offcanvas offcanvas-end shadow-5-strong" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
 
@@ -27,21 +27,9 @@
                     </li>
                     @else
                     @endauth
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href=""><i class="bi bi-calendar-event"></i></i>General Event</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href=""><i class="bi bi-cup-straw"></i>Food & Drink</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href=""><i class="bi bi-file-earmark-richtext"></i>Blogs</a>
-                    </li>
+                 
                 </ul>
-                <form class="d-flex mt-3" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-success" type="submit">Search</button>
-                </form>
+              
             </div>
         </div>
     </div>
