06dc3475282aff464de550719026728b408e60b4
julzdoug <jschwarzzh@hotmail.com>
2023-04-04 14:31:32 +0200

    profile navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,31 @@
 <nav class="navbar navbar-dark shadow-5-strong">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="{{asset('images/justDumplingLogo.png') }}" alt="dumpling blog logo" width="150" height="70"></a>
+        {{--  From her the Login and profile header start  --}}
+        <ul class=""><i class="bi bi-person-circle"></i>
+        <a class="nav-link text-dark" href="#"><li class="nav-item"></a>
+            @auth
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#">{{ Auth::user()->name }}</a>
+                </li>
+                <li class="nav-item">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-link text-dark"><i class="bi bi-door-closed"></i>Logout</button>
+                    </form>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
+                    </li>
+                </ul>
+                @endif
+            @endauth
+        </ul>
+        
         <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -15,18 +40,29 @@
                     <li class="nav-item">
                         <a class="nav-link active text-dark" aria-current="page" href="{{ url('/homePage') }}"><i class="bi bi-house"></i>Home</a>
                     </li>
-                    <li class="nav-item">
+                    {{--  Here the login credentialswould be in the hamburger menu
+                        
+                        <li class="nav-item">
                         @auth
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
-                    </li>
-                    <li class="nav-item">
-                        @if (Route::has('register'))
-                        <a class="nav-link text-dark" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
-                        @endif
-                    </li>
-                    @else
-                    @endauth
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="#">{{ Auth::user()->name }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <form action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="btn btn-link text-dark"><i class="bi bi-door-closed"></i>Logout</button>
+                                </form>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link text-dark" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
+                                </li>
+                            @endif
+                        @endauth  --}}
                  
                 </ul>
               
