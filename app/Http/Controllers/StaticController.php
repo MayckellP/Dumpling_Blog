497ed25ae1d77ce55016eb83bd4cf314ff9ffb6a
Soualif <electlikon@gmail.com>
2023-04-05 08:45:13 +0200

    commit-05-April-23

---
+++ app/Http/Controllers/StaticController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticController extends Controller
+{
+    // public function index){
+    //     return view('index');
+    // }
+    
+    public function home(){
+        return view('home');
+    }
+    public function about(){
+        return view('about-us');
+    }
+
+    public function contact(){
+        return view('contact-us');
+    }
+
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+       <!DOCTYPE html>
+       <html lang="en">
+       <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+       </head>
+       <body>
+       <div class="reletive py-4 px-6">
+            <nav>
+                <a href="/home">Home</a>
+                <a href="/about">About-us</a>
+                <a href="/contact">Contact-us</a>
+            </nav>
+        </div>
+
+        <div class="relative flex items-top justify-center">
+            @yield('content')
+        </div>
+       </body>
+       </html>
