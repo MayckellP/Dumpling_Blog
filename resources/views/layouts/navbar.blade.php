631800d91d21be5d506d735334ed4f3f6747ef42
julzdoug <jschwarzzh@hotmail.com>
2023-04-05 10:13:11 +0200

    profile avatar and navbar collapse

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,15 @@
-<nav class="navbar navbar-dark shadow-5-strong">
+<nav class="navbar navbar-dark shadow-5-strong fixed-top navbar-expand-sm">
     <div class="container-fluid">
         {{--  From her the Login and profile header start  --}}
-        <ul class=""><i class="bi bi-person-circle"></i>
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-circle"></i>
+          </button>
+        <ul class="dropdown-menu">
         <a class="nav-link text-dark" href="#"><li class="nav-item"></a>
             @auth
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="#">{{ Auth::user()->name }}</a>
+                    <a class="nav-link text-dark ms-2" href="#"><i class="bi bi-person-circle"></i>{{ Auth::user()->name }}</a>
                 </li>
                 <li class="nav-item">
                     <form action="{{ route('logout') }}" method="POST">
@@ -15,52 +19,52 @@
                 </li>
             @else
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
+                    <a class="nav-link text-white" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
                 </li>
                 @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
+                        <a class="nav-link text-white" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
                     </li>
-                </ul>
+               
                 @endif
             @endauth
         </ul>
-        
-        <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+    </div>
+        <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-end shadow-5-strong" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5>
-                <button type="button" class="btn-close bg-primary" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                {{--  <button type="button" class="btn-close bg-primary" data-bs-dismiss="offcanvas" aria-label="Close"></button>  --}}
             </div>
 
             <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-2">
                     <li class="nav-item">
-                        <a class="nav-link active text-dark" aria-current="page" href="{{ url('/homePage') }}"><i class="bi bi-house"></i>Home</a>
+                        <a class="nav-link active text-white" aria-current="page" href="{{ url('/homePage') }}"><i class="bi bi-house"></i>Home</a>
                     </li>
-                    <li class="nav-item">
-                        @auth
+                    {{-- <li class="nav-item">
+                         @auth
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="#">{{ Auth::user()->name }}</a>
+                                <a class="nav-link text-white" href="#">{{ Auth::user()->name }}</a>
                             </li>
                             <li class="nav-item">
                                 <form action="{{ route('logout') }}" method="POST">
                                     @csrf
-                                    <button type="submit" class="btn btn-link text-dark"><i class="bi bi-door-closed"></i>Logout</button>
+                                    <button type="submit" class="btn btn-link text-white"><i class="bi bi-door-closed"></i>Logout</button>
                                 </form>
                             </li>
                         @else
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
+                                <a class="nav-link text-white" href="{{ route('login') }}"><i class="bi bi-door-open"></i>Login</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link text-dark" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
+                                    <a class="nav-link text-white" href="{{ route('register') }}"><i class="bi bi-card-checklist"></i>Register</a>
                                 </li>
                             @endif
-                        @endauth
+                        @endauth  --}}
                  
                 </ul>
               
