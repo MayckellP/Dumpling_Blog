89ce1c64f1f001b649a279155c6e1f97e184a641
MayckellP <mayckell-perez@hotmail.com>
2023-04-05 09:17:51 +0200

    Background video

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,33 @@ function like() {
 }
 
 function dislike() {}
+
+function selectEvent() {
+    var event = document.getElementById("event");
+    var food = document.getElementById("food");
+    var club = document.getElementById("club");
+    var blog = document.getElementById("blog");
+    var allOk = document.getElementById("all_ok");
+    var content = document.getElementById("cont-calendar");
+    var link = document.getElementById("link-calendar");
+
+    if (event.checked || food.checked || club.checked || blog.checked) {
+        content.className = "hidden";
+        content.style.display = "none";
+
+        link.className = "hidden";
+        link.style.display = "none";
+
+        allOk.style.display = "inherit";
+        allOk.className = "visible";
+    } else {
+        allOk.style.display = "none";
+        allOk.className = "hidden";
+
+        content.className = "visible";
+        content.style.display = "inherit";
+
+        link.className = "visible";
+        link.style.display = "block";
+    }
+}
