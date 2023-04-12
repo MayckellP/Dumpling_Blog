bae8b37412a4c0ba8bb1c7d5132d24d0a80d41d7
Soualif <electlikon@gmail.com>
2023-04-12 10:02:37 +0200

    commoit translate

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,23 @@
+const axios = require("axios");
+
+const options = {
+  method: 'POST',
+  url: 'https://rapid-translate-multi-traduction.p.rapidapi.com/t',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '038c4147dbmsh79947fb14f079edp11f9a2jsn3a12d55c49b5',
+    'X-RapidAPI-Host': 'rapid-translate-multi-traduction.p.rapidapi.com'
+  },
+  data: '{"from":"en","to":"ar","e":"","q":["Hello","Whats","<h1>Test</h1>","Translate API","Rapid Translate Multi Traduction"]}'
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+
 function like() {
     var like = document.getElementById("checklike");
     var likeValue = document.getElementById("checkValue").value;
--- resources/views/components/translateButton.blade.php
+++ resources/views/components/translateButton.blade.php
@@ -1,4 +1,15 @@
-<style>
+
+
+</script>
+<!DOCTYPE html>
+<html lang="en-US">
+ 
+<head>
+    <title>
+        How To Add Google Translate
+        Button On Your Webpage ?
+    </title>
+    <style>
     .btn-translate {
       width:50px;
       height:50px;
@@ -16,6 +27,17 @@
         filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5));
     }
 </style> 
+</head>
+ 
+<body>
+    <p>Hello everyone!</p>
+    <p>Welcome to GeeksforGeeks</p>
+ 
+    <p>
+        Translate this page in
+        your preferred language:
+    </p>
+    
 <div class="btn-translate dropend d-flex justify-content-start  text-white m-3">
     <button type="button" class="btn-translate text-end" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="/images/translate.svg">
@@ -33,3 +55,30 @@
       <li><hr class="dropdown-divider"></li>
     </ul>
 </div>
+
+
+
+
+    <div id="google_translate_element"></div>
+ 
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement(
+                {pageLanguage: 'en'},
+                'google_translate_element'
+            );
+        }
+    </script>
+ 
+    <script type="text/javascript"
+            src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+    </script>
+ 
+    <p>
+        You can translate the content of this
+        page by selecting a language in the
+        select box.
+    </p>
+</body>
+ 
+</html>
