8bacb32ea750b19009e2e0a2b34c092e94f913d6
julzdoug <jschwarzzh@hotmail.com>
2023-04-13 10:52:58 +0200

    working on delet button

--- resources/views/publications/blogcard.blade.php
+++ resources/views/publications/blogcard.blade.php
@@ -1,26 +1,25 @@
 <div class="container">
     <div class="cards grid-row">
+        @foreach($publicationsDetails as $publicationsDetails)
         <div class="card">
             <div class="card-top">
-                <img src="img3.jpg" alt="Blog Name">
+                <img src="{{ $publicationsDetails->image }}" alt="{{ $publicationsDetails->title }}">
             </div>
             <div class="card-info">
-                <h2>How to Build Signature Pad</h2>
-                <span class="date">Monday, Jan 20, 2021</span>
-                <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s</p>
+                <h2>{{ $publicationsDetails->title }}</h2>
+                <span class="date">{{ $publicationsDetails->created_at->format('l, M j, Y') }}</span>
+                <p class="excerpt">{{ $publicationsDetails->excerpt }}</p>
             </div>
             <div class="card-bottom flex-row">
-                <a href="#" class="read-more">Read Full Blog</a>
-                <a href="#" class="button btn-dpink">Blog</a>
+                <a href="{{ route('publications.show', $publicationsDetails->id) }}" class="read-more">Read Full Publication</a>
+                <a href="{{ $publicationsDetails->link }}" class="button btn-dpink">Publication</a>
             </div>
         </div>
+        @endforeach
     </div>
-</div>
 </div>
 @auth
 <a href="/create">
     <button type="button" class="btn btn-success m-3">Create Event</button>
 </a>
 @endauth
-</div>
-</div>
