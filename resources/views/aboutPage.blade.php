8fa5d04a74178aa944a040348844068b0312beb0
julzdoug <jschwarzzh@hotmail.com>
2023-04-18 12:19:09 +0200

    about us

--- resources/views/aboutPage.blade.php
+++ resources/views/aboutPage.blade.php
@@ -1,6 +1,20 @@
 <x-app-layout>
+    <div class="cont-home">
 
-
-
+        <!-- VIDEO BACKGROUND  -->
+        <video src="/images/background_mobile.mp4" 
+            autoplay="true" 
+            muted="true" 
+            loop="true" 
+            poster="https://carontestudio.com/img/contacto.jpg">
+        </video>
+        <section class="about-group-desktop">
+            <div class="text-center"><h1>About Us</h1>
+               
+                <h2>Starting at 2023. We are dedicate with are aproved Blogger and Event Managers to let you know what you want to know about the EventLife
+                </h2></div>
+                </section>
+    </div>
+    
         
 </x-app-layout>
